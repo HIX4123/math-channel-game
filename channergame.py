8ae3b133287ea7e4ca37cf0280850cc1c2dfc179
HIX_21A151921-004123 <rnjsalsrb6666@naver.com>
2021-12-09 11:03:04 +0900

    commit

---
+++ channergame.py
@@ -0,0 +1,9 @@
+turnDict = {0 : 'A', 1 : 'B'} # 0 : A, 1 : B
+turn = 0
+
+num = 1
+nummax = 31
+
+while num < nummax :
+    if input(f"{turnDict[turn]}, input s to +1 or m to x2 : ") == 's' :
+        print("asdf")
