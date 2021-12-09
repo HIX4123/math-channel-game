66fe57c21b876899a5df83845f90a6ef6031abee
HIX_21A151921-004123 <rnjsalsrb6666@naver.com>
2021-12-09 11:39:06 +0900

    commit

--- channergame.py
+++ channergame.py
@@ -27,3 +27,54 @@ while num < nummax :
     wrong = 0
     turn = (turn + 1) % 2
 print(f"Game is over for {turnDict[(turn + 1) % 2]} to overflow the given number '{num}'.\nThe winner is {turnDict[turn]}!")
+print("""                                                                                      
+                                                                                      
+                                                                                      
+                                                                                      
+                                                                                      
+                                                                                      
+                                                                                      
+                                                                                      
+                                                                                      
+                                                                                      
+                                           L                                          
+                                     Jr   BB;   M                                     
+                                    :BB   61H  5BB                                    
+                                     :,    .    ;                                     
+                                           :    .                                     
+                                           ,    ,                                     
+                                     .     :    :                                     
+                                           ,                                          
+                             :L1HEMBB .BBB. HBBM ;BBR6UJr.                            
+                          BBBBBBBBBBBBBBBBL,BBBBBBBBBBBBBBBBv                         
+                          BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB                         
+                          BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB                         
+                          BP .BBBBBBBBBBBBBBBBBBBBBBBBBBR .BK                         
+                               BBB7   .BBBBBBBBE    OBBB                              
+                                         JORZr                                        
+                      iBBB,                                 5BBg,                     
+                    QBRB5B2    7:                     v:    BgBBBB:                   
+                    BBBRBBBBBBKBBJaJL7vvr:BBc;r77LJ2XJBBZBBBBBRQBBB                   
+                    BBBBBBBBBBBXaBBBBBBBBB5gDBBBBBBBBBaKBBBBBBBBBBB                   
+                   ,BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB                   
+                   ,BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB                   
+                    BBLvBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB6rHBB                   
+                    ;     XBBBBBU    ,RBBBBBBBBBJ    ,DBBBBBL    ..                   
+                            .:          :XgDOL.         .:                            
+                                                                                      
+                  ,:                                              .:.                 
+              .r;;c1,                   ...   ...                 ;L7ri;              
+              :s:,::r:,.                                      ..:rr:,:rJ              
+                ,rrrrrrrr;;;::,,,.......          .....,:::::;rrr;r7r;.               
+                     .:;rr7L7vvL77r77rrrrrrrrrrrr77777LLLLL77ri::.                    
+                                ....,:::::::::::::,,..                                
+                                                                                      
+                                                                                      
+                                                                                      
+                                                                                      
+                                                                                      
+                                                                                      
+                                                                                      
+                                                                                      
+r                                                                                     """)
+input("press any key to quit...")
