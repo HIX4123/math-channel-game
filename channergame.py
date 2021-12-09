d74bb658cd01104e26413b50dc3c56e5944f0bf9
HIX_21A151921-004123 <rnjsalsrb6666@naver.com>
2021-12-09 11:07:43 +0900

    commit

--- channergame.py
+++ channergame.py
@@ -5,5 +5,10 @@ num = 1
 nummax = 31
 
 while num < nummax :
-    if input(f"{turnDict[turn]}, input s to +1 or m to x2 : ") == 's' :
-        print("asdf")
+    command = input(f"{turnDict[turn]}, input s to +1 or m to x2 : ")
+    if command == 's' :
+        num += 1
+        print(f"conducted +1, now num is '{num}'")
+    elif command == 'm' :
+        num *= 2
+        print(f"conducted x2, now num is '{num}'")
