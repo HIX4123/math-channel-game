d7d7826f42d89fd7d4b73c8e7d20a05cb295f6c9
HIX_21A151921-004123 <rnjsalsrb6666@naver.com>
2021-12-09 11:23:50 +0900

    commit

--- channergame.py
+++ channergame.py
@@ -4,11 +4,24 @@ turn = 0
 num = 1
 nummax = 31
 
+wrong = 0
+
+print(f"The given number is '{num}'")
 while num < nummax :
     command = input(f"{turnDict[turn]}, input s to +1 or m to x2 : ")
     if command == 's' :
         num += 1
-        print(f"conducted +1, now num is '{num}'")
+        print(f"conducted +1, now num is '{num}'\n")
     elif command == 'm' :
         num *= 2
-        print(f"conducted x2, now num is '{num}'")
+        print(f"conducted x2, now num is '{num}'\n")
+    else :
+        print("you gave me something wrong, input s to +1 or m to x2 again\n")
+        wrong += 1
+        if wrong >= 3 :
+            print("Jesus f**k, Quit Turkey your trolling. You loser, B**h")
+            quit()
+        continue
+    wrong = 0
+    turn = (turn + 1) % 2
+print(f"Game is over for {turnDict[(turn + 1) % 2]} to overflow the given number '{num}'.\nThe winner is {turnDict[turn]}!")
